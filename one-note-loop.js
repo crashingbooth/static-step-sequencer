0c9ba295a5b13cfeee052ac3265274ae40151e76
crashing booth <jholtzkener@hotmail.com>
2022-02-27 08:36:23 -0500

    refactors line creation to object bundle

--- one-note-loop.js
+++ one-note-loop.js
@@ -8,48 +8,57 @@ document.getElementById("action-button").addEventListener("click", function() {
   play();
 });
 
+document.getElementById("stop-button").addEventListener("click", function() {
+  Tone.Transport.stop();
+});
+
 const bPat1 = [1,0,0,0, 1,1,0,0, 1,0,0,0, 1,1,0,0];
 const bPat2 = [0,0,1,0, 0,0,1,1, 0,0,1,0, 0,0,1,1];
+const notes = ["C3", "E4"];
 const patterns = [bPat1, bPat2];
+const synthA = new Tone.MembraneSynth().toDestination();
+const synthB = new Tone.MembraneSynth().toDestination();
+const synths = [synthA, synthB];
+
 
 const originalDiv = document.getElementById("dots-go-here");
-const replacement = document.createElement('div');
 
-let dots = [];
-for (let pat of patterns) {
-	console.log("pat");
-	let subDiv = document.createElement('div');
-	let dotLine = [];
-	for (let i = 0; i < 16; i++) {
-	  const dot = document.createElement('span')
-	  dot.className =  pat[i] ? 'on-dot' : 'off-dot';
-		dotLine.push(dot);
-	  subDiv.appendChild(dot);
-	}
-	dots.push(dotLine);
-	replacement.appendChild(subDiv);
-}
-originalDiv.parentNode.replaceChild(replacement, originalDiv);
 
 function createDotsFromPattern(pattern) {
 	let subDiv = document.createElement('div');
 	for (let i = 0; i < pattern.length; i++) {
 	  const dot = document.createElement('span')
-	  dot.className =  pat[i] ? 'on-dot' : 'off-dot';
+	  dot.className =  pattern[i] ? 'on-dot' : 'off-dot';
 	  subDiv.appendChild(dot);
 	}
 	return subDiv;
 }
 
+function createObjectBundleFromPattern(pat, note, synth) {
+	return { dotsDiv: createDotsFromPattern(pat),
+	 				 pattern: pat,
+					 note: note,
+					 synth: synth,
+				}
+}
+
+let lineBundles = [];
+let replacement = document.createElement('div');
+for (let i = 0; i < patterns.length; i++) {
+	let lineBundle = createObjectBundleFromPattern(patterns[i], notes[i], synths[i]);
+	console.log(lineBundle);
+	replacement.appendChild(lineBundle.dotsDiv);
+	lineBundles.push(lineBundle);
+}
+originalDiv.parentNode.replaceChild(replacement, originalDiv);
+
 
 let i = 0;
 const play = () => {
-  const synthA = new Tone.MembraneSynth().toDestination();
-  const synthB = new Tone.MembraneSynth().toDestination();
-
   const loopA = new Tone.Loop((time) => {
-		processLine(patterns[0],dots[0], synthA, "C3", i, time);
-		processLine(patterns[1],dots[1], synthB, "E4", i, time);
+		for (let lineBundle of lineBundles) {
+			processLineBundle(lineBundle, i, time);
+		}
     i += 1;
     if (i >= 16) { i = 0};
   }, "16n").start(0);
@@ -58,9 +67,11 @@ const play = () => {
 };
 
 
-function processLine(booleanLine, dots, synth, note, i, time) {
-	if (booleanLine[[i % 16]]) {
-		synth.triggerAttackRelease(note, "16n", time);
+function processLineBundle(lineBundle, i, time) {
+	// let {pattern, dotsDiv, note, synth} = lineBundle;
+	let dots = lineBundle.dotsDiv.children;
+	if (lineBundle.pattern[[i % 16]]) {
+		lineBundle.synth.triggerAttackRelease(lineBundle.note, "16n", time);
 		dots[i % 16].classList.remove('dot--once');
     void dots[i % 16].offsetWidth;
     dots[i % 16].classList.add('dot--once');
